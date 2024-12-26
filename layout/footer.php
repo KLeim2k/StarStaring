3fb1f456104fe47902107559fdc562f6e3659fa2
KLeim2k <114597624+KLeim2k@users.noreply.github.com>
2024-12-26 17:35:20 +0500

    Add files via upload

---
+++ layout/footer.php
@@ -0,0 +1,69 @@
+    <!--footer-->
+    <footer class="mt-5 py-5">
+      <div class="row container mx-auto pt-5">
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <img class="logo" src="assets/img/logo1.png" alt="#">
+          <p class="pt-3">Мы представляем лучшие сигнализации на рынке по приемлемым ценам</p>
+        </div>
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <h5 class="pb-2">Популярные</h5>
+          <ul class="text-uppercase">
+            <li><a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">men</a></li>
+            <li><a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">ananas</a></li>
+            <li><a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">banana</a></li>
+            <li><a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">zalupa</a></li>
+            <li><a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">potato</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <h5 class="pb-2">Контакты</h5>
+          <div>
+            <h6 class="text-uppercase">Адрес</h6>
+            <p>г.Екатеринбург, ул.Залупица, д.52</p>
+          </div>
+          <div>
+            <h6 class="text-uppercase">Телефон</h6>
+            <p>+7(929)334-43-43</p>
+          </div>
+          <div>
+            <h6 class="text-uppercase">Почта</h6>
+            <p>info@email.com</p>
+          </div>
+        </div>
+        
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <h5 class="pb-2">Инстаграм</h5>
+          <div class="row">
+            <img class="img-fluid w-25 h-100 m-2" src="assets/img/footer1.jpg" alt="#">
+            <img class="img-fluid w-25 h-100 m-2" src="assets/img/footer2.jpg" alt="#">
+            <img class="img-fluid w-25 h-100 m-2" src="assets/img/footer3.jpg" alt="#">
+            <img class="img-fluid w-25 h-100 m-2" src="assets/img/footer4.jpg" alt="#">
+            <img class="img-fluid w-25 h-100 m-2" src="assets/img/footer5.png" alt="#">
+          </div>
+        </div>
+      </div>
+
+      <div class="copyright mt-5">
+        <div class="row container mx-auto">
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+            <img src="assets/img/cards.png" alt="#">
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-4 text-nowrap mb-2">
+            <p>© 2024 «StarStaring». Все права защищены</p>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><i class="fab fa-vk"></i></a>
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><i class="fab fa-whatsapp"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>    
+</body>
+</html>
---
+++ layout/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="StarStaring предлагает лучшие автосигнализации по доступным ценам в Екатеринбурге. Популярные товары, автозапуск, управление с телефона.">
+    <meta name="keywords" content="Екатеринбург, автосигнализация, автозапуск, управление с телефона, лучшие цены, StarStaring, Starline, StarLine, сигнализация, ананасы, A90, S96, Глонасс, BMW, Toyota, бананы">
+    <meta name="author" content="StarStaring">
+    <title>StarStaring | Купить автосигнализацию Starline в Екатеринбурге</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <script src="https://kit.fontawesome.com/2824928ee5.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="assets/css/style.css">    
+</head>
+<body>
+
+    <!--Навигация boostrap-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
+        <div class="container">
+          <img class="logo" src="assets/img/logo1.png" alt="#">
+          <a class="brd" href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><h2 class="brand">StarStaring</h2></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Главная</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="shop.php">Товары</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Контакты</a>
+              </li>
+
+              <li class="nav-item">
+                <a href="cart.php"><i class="fas fa-solid fa-cart-shopping fa-lg"></i></a>
+                <a href="account.php"><i class="fas fa-solid fa-user fa-lg"></i></a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
